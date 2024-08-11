72526a2553e69c40a4fb49238711055731e08484
ItsDuy <duy.nguyen280805@hcmut.edu.vn>
2024-08-11 17:53:23 +0700

    Updated Program.cs to include new features

--- Program.cs
+++ Program.cs
@@ -9,9 +9,20 @@ class Program
             Console.WriteLine("Enter the year:");
             int year;
             int.TryParse(Console.ReadLine(), out year);
-            if(year%4==0)
+            if (year % 4 == 0)
             {
-                Console.WriteLine("Its a leap year");
+                if (year % 400 == 0)
+                {
+                    Console.WriteLine("Its a leap year");
+                }
+                else if (year % 100 == 0)
+                {
+                    Console.WriteLine("Its not a leap year");
+                }
+                else
+                {
+                    Console.WriteLine("Its a leap year");
+                }
             }
             else
             {
