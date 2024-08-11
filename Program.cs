a81332d7075ac4b41dfaac73995a7d998284766f
ItsDuy <duy.nguyen280805@hcmut.edu.vn>
2024-08-11 17:05:42 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LeapYear;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Enter the year:");
+            int year;
+            int.TryParse(Console.ReadLine(), out year);
+            if(year%4==0)
+            {
+                Console.WriteLine("Its a leap year");
+            }
+            else
+            {
+                Console.WriteLine("Its not a leap year");
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Error! Please try again later.");
+        }
+    }
+}
